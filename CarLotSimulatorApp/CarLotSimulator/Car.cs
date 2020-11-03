fb79ce7c73797ffaf73fde80c4c8ca25377a158c
bluebeacon8402 <crsorge@icloud.com>
2020-11-03 11:12:43 -0500

    new exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable - DONE
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public bool EngineNoise { get; set; }
+        public bool HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+
+        public void MakeEngineNoise()
+        {
+            if (EngineNoise == true)
+            {
+                Console.WriteLine("Vroom vroom");
+            }
+            else
+            {
+                Console.WriteLine("Put put");
+            }
+        }
+
+        public void MakeHonkNoise()
+        {
+            if (HonkNoise == true)
+            {
+                Console.WriteLine("Awooga!");
+            }
+            else
+            {
+                Console.WriteLine("beep beep");
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -11,12 +11,23 @@ namespace CarLotSimulator
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //The methods should take one string parameter: the respective noise property - I did not do this - Bool instead.
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //Now that the Car class is created we can instanciate 3 new cars - DONE
+            var Car1 = new Car() { Year = 1988, Make = "Nissan", Model = "Pathfinder", EngineNoise = true, HonkNoise = true, IsDriveable = true };
+            var Car2 = new Car() { Year = 2000, Make = "Ford", Model = "Focus", EngineNoise = false, HonkNoise = false, IsDriveable = true };
+            var Car3 = new Car() { Year = 2019, Make = "Chevy", Model = "Impalla", EngineNoise = true, HonkNoise = false, IsDriveable = true };
+            //Set the properties for each of the cars - DONE
+
+            //Call each of the methods for each car - DONE
+            Car1.MakeEngineNoise();
+            Car2.MakeEngineNoise();
+            Car3.MakeEngineNoise();
+
+            Car1.MakeHonkNoise();
+            Car2.MakeHonkNoise();
+            Car3.MakeHonkNoise();
 
             //*************BONUS*************//
 
